8511efff8542c2f441632c58bec8ae75090538ef
nana <1489591380@qq.com>
2025-04-02 23:48:09 +0800

    数列

---
+++ .vscode/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int result = fibonacci(n);
+        System.out.println("斐波那契数列的第 " + n + " 项是: " + result);
+    }
+}    
